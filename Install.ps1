de1f397c408c1a4b73d59800a9ce5a3c80baafdc
Daniel Potter <Daniel@DPotter.net>
2020-07-04 23:17:58 -0700

    Update documentation for the Install script

--- Install.ps1
+++ Install.ps1
@@ -1,72 +1,80 @@
 [CmdletBinding(SupportsShouldProcess)]
 param ()
 
-begin
-{
-    $localProfilesPath = Join-Path $PSScriptRoot Profiles
-    $configFilePath = Join-Path $localProfilesPath ProfileConfig.jsonc
-    $defaultConfigFilePath = Join-Path $PSScriptRoot ProfileConfig.Default.jsonc
-
-    function getProfileNames
-    {
-        $configContent = Get-Content $configFilePath -ErrorAction SilentlyContinue
-        if ($configContent)
-        {
-            $config = $configContent | ConvertFrom-Json
-        }
-        else
-        {
-            $config = Get-Content $defaultConfigFilePath | ConvertFrom-Json
-        }
-
-        return $config.profiles | Select-Object -ExpandProperty name
-    }
-}
-
 process
 {
-    # Install config files.
-    if (-not (Test-Path $localProfilesPath))
+    $personalProfileDirectoryPath = Join-Path $PSScriptRoot Profiles
+    $personalConfigFilePath = Join-Path $personalProfileDirectoryPath ProfileConfig.jsonc
+    $defaultConfigFilePath = Join-Path $PSScriptRoot ProfileConfig.Default.jsonc
+
+    # Create the Profiles directory if it doesn't already exist.
+    if (-not (Test-Path $personalProfileDirectoryPath))
     {
-        New-Item $localProfilesPath -ItemType Directory | Out-Null
+        New-Item $personalProfileDirectoryPath -ItemType Directory | Out-Null
     }
 
-    if (-not (Test-Path $configFilePath) -or -not (Get-Content $configFilePath))
+    # Clone the default config if the personal config is empty.
+    if (-not (Test-Path $personalConfigFilePath) -or (Get-Item $personalConfigFilePath).Length -eq 0)
     {
         Get-Content $defaultConfigFilePath | ForEach-Object {
+            # Replace the schema reference for the personal config file
+            # because it will be placed in a child directory.
             $_ -replace [regex]::Escape('"$schema": "./ProfileConfig.Schema.json"'), '"$schema": "../ProfileConfig.Schema.json"'
-        } | Set-Content $configFilePath -ErrorAction Inquire
+        } | Set-Content $personalConfigFilePath -ErrorAction Inquire
     }
 
-    # Install profiles.
-    $profileDirectory = Split-Path $profile
+    # Create the script that will be sourced for all profiles.
+    $personalScriptStubPath = Join-Path $personalProfileDirectoryPath AllProfiles.ps1
+    if (-not (Test-Path $personalScriptStubPath))
+    {
+        New-Item $personalScriptStubPath -ItemType File | Out-Null
+    }
+
+    # Grab the directory of the PowerShell profile directory.
+    # This is usually in the user's home directory and differs
+    # between Windows PowerShell and PowerShell core.
+    $profileDirectory = Split-Path $PROFILE
+
+    # Create the PowerShell profile directory if needed.
     if (-not (Test-Path $profileDirectory))
     {
-        New-Item $profileDirectory -ItemType Directory -Force
+        New-Item $profileDirectory -ItemType Directory | Out-Null
     }
 
-    $localScriptPath = Join-Path $localProfilesPath AllProfiles.ps1
-    if (-not (Test-Path $localScriptPath))
+    # Get the config data.
+    $configContent = Get-Content $personalConfigFilePath -ErrorAction SilentlyContinue
+    if ($configContent)
     {
-        New-Item $localScriptPath -ItemType File | Out-Null
+        $config = $configContent | ConvertFrom-Json
+    }
+    else
+    {
+        # Fall back to the default config file.
+        # Since the config file was just cloned, this is here to allow this
+        # script to run with -WhatIf where Set-Content won't actually run.
+        $config = Get-Content $defaultConfigFilePath | ConvertFrom-Json
     }
 
-    getProfileNames | ForEach-Object {
+    # Initialize all configured profiles.
+    $config.profiles | Select-Object -ExpandProperty name | ForEach-Object {
         [string] $profileName = $_
 
+        $profileScriptName = $profileName + "_profile.ps1"
+
+        # Create a personal profile stub if needed.
+        $personalScriptStubPath = Join-Path $personalProfileDirectoryPath $profileScriptName
+        if (-not (Test-Path $personalScriptStubPath))
+        {
+            New-Item $personalScriptStubPath -ItemType File | Out-Null
+        }
+
+        # Set up the content for profiles.
         $profileContent = @(
             "`$Global:PSProfile = '$profileName'"
             ". '$PSScriptRoot\Profile.ps1'"
         )
 
-        $profileScriptName = $profileName + "_profile.ps1"
-
-        $localScriptPath = Join-Path $localProfilesPath $profileScriptName
-        if (-not (Test-Path $localScriptPath))
-        {
-            New-Item $localScriptPath -ItemType File | Out-Null
-        }
-
+        # Set content of the PowerShell profile script.
         $profileScriptPath = Join-Path $profileDirectory $profileScriptName
         $profileContent | Set-Content $profileScriptPath -Force -ErrorAction Inquire
     }
